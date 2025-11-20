8e434e89c15756325ff80e4e7dc9b9532220f0a0
Velimir Radlovački <velimir.radlovacki@gmail.com>
2025-11-20 08:09:46 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.Write("Unesi A: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Unesi B: ");
+            b = int.Parse(Console.ReadLine());
+            c = a + b;
+            Console.WriteLine("Zbir A i B je: " + c);
+        }
+    }
+}
